615c19d228bfdcfdb1318d20259e45ebdc5f0a29
sandmandreamcatcher <dr.catcher@ukr.net>
2021-07-30 17:58:00 +0300

    Edit prefabs

---
+++ Assets/Scripts/ObjectPlacer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ObjectPlacer : MonoBehaviour
+{
+    [SerializeField] private Transform _objectPlace;
+    [SerializeField] private Camera _camera;
+    [SerializeField] private GameObject _container;
+
+    private ARRaycastManager _arRaycastManager;
+    private GameObject _installedObject;
+    private List<ARRaycastHit> _hits = new List<ARRaycastHit>();
+
+    private void Start()
+    {
+        _arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    public void SetInstalledObject(ItemData itemData)
+    {
+        if (_installedObject != null)
+            Destroy(_installedObject);
+
+        _installedObject = Instantiate(itemData.Prefab, _objectPlace);
+    }
+}
--- Assets/Scripts/SelectionPanel.cs
+++ Assets/Scripts/SelectionPanel.cs
@@ -1,34 +1,29 @@
 ﻿using UnityEngine;
 
 public class SelectionPanel : MonoBehaviour
-{
-    [SerializeField] private Transform _objectPlace;
-    [SerializeField] private GameObject _itemTemplate;
+{    
     [SerializeField] private ItemData[] _itemDatas;
+    [SerializeField] private ObjectPlacer _objectPlacer;
+    [SerializeField] private GameObject _itemTemplate;
     [SerializeField] private Transform _container;
 
     private void Start()
     {
         for (int i = 0; i < _itemDatas.Length; i++)
-        {
             AddItem(_itemDatas[i]);
-            
-        }
-        
-        
     }
 
     private void AddItem(ItemData itemData)
     {
-      Instantiate(_itemTemplate, _container).TryGetComponent(out ItemView itemView);
-      itemView.Initialize(itemData);
-      itemView.ItemSelected += OnItemSelected;
-      itemView.ItemDisabled += OnItemDisabled;
+        Instantiate(_itemTemplate, _container).TryGetComponent(out ItemView itemView);
+        itemView.Initialize(itemData);
+        itemView.ItemSelected += OnItemSelected;
+        itemView.ItemDisabled += OnItemDisabled;
     }
 
     private void OnItemSelected(ItemData itemData)
     {
-        
+        _objectPlacer.SetInstalledObject(itemData);
     }
 
     private void OnItemDisabled(ItemView itemView)
