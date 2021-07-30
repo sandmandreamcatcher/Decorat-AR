eef6e8c473bad4f66cf6d0584396dd7da8b73562
sandmandreamcatcher <dr.catcher@ukr.net>
2021-07-30 19:13:36 +0300

    ChangedPrefabs

--- Assets/Scripts/ObjectPlacer.cs
+++ Assets/Scripts/ObjectPlacer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 [RequireComponent(typeof(ARRaycastManager))]
 public class ObjectPlacer : MonoBehaviour
@@ -18,11 +19,54 @@ public class ObjectPlacer : MonoBehaviour
         _arRaycastManager = GetComponent<ARRaycastManager>();
     }
 
+    private void Update()
+    {
+        UpdatePlacementPose();
+
+        if (Input.touchCount == 2)
+        {
+            SetObject();
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        Vector2 screenCenter = _camera.ViewportToScreenPoint(new Vector2(0.1f, 0.1f));
+
+        var ray = _camera.ScreenPointToRay(screenCenter);
+
+        if (Physics.Raycast(ray, out RaycastHit raycatHit))
+        {
+           SetObjectPosition(raycatHit.point);
+        }
+        else if (_arRaycastManager.Raycast(screenCenter, _hits, TrackableType.PlaneWithinPolygon))
+        {
+            SetObjectPosition(_hits[0].pose.position);
+        }
+    }
+
+    private void SetObjectPosition(Vector3 position)
+    {
+        _objectPlace.position = position;
+        Vector3 cameraForward = _camera.transform.forward;
+        Vector3 cameraRotation = new Vector3(cameraForward.x, 0, cameraForward.z);
+        _objectPlace.rotation = Quaternion.Euler((cameraRotation));
+    }
+    
+    private void SetObject()
+    {
+        _installedObject.GetComponent<Collider>().enabled = true;
+        _installedObject.transform.parent = _container.transform;
+        _installedObject = null;
+    }
     public void SetInstalledObject(ItemData itemData)
     {
         if (_installedObject != null)
             Destroy(_installedObject);
 
         _installedObject = Instantiate(itemData.Prefab, _objectPlace);
+        _installedObject.GetComponent<Collider>().enabled = false;
     }
+
+
 }
